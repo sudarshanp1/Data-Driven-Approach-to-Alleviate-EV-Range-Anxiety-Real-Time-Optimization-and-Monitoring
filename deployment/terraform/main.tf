c5f9152789de8db7e5a8479fabd36ce9005ac3d5
Sudarshan P <121892659+sudarshanp1@users.noreply.github.com>
2024-11-17 14:04:49 +0530

    Add files via upload

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,1 +1,10 @@
 
+# Terraform script for provisioning AWS infrastructure
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
+                
